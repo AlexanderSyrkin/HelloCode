98c1188e3df5daff5d9a2fd1cf9e324525bb6de0
AlexSyrkin <hellius2031@mail.ru>
2022-10-12 21:07:07 +0200

    add Examle 009 IntroArray

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -5,7 +5,7 @@ Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 if (number % 2 ==0)
 {
-  Console.WriteLine("Число чётное");
+ Console.WriteLine("Число чётное");
 }
 else
 {
