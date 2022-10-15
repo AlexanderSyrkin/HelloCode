a385fea3e7d5f803eb544f5efc9c9108d8ec720e
AlexSyrkin <hellius2031@mail.ru>
2022-10-15 13:58:21 +0200

    add Homework Seminer 2 Task 13

---
+++ Homework_Seminar2_Task_13/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int a1 = number;
+
+if (number < 100)
+{
+    Console.WriteLine($"{number} -> Третьей цифры нет");
+}
+else if (a1 > 100000000)
+{
+    Console.WriteLine("ОШИБКА!!! Слишком большое значение - введите число меньшего размера!");
+}
+else if (a1 > 10000000)
+{
+    a1 = number/100000;
+    int a3 = ((a1%100)/1)%10;
+    Console.WriteLine($"{number} -> {a3}");
+}
+else if (a1 > 1000000)
+{
+    a1 = number/10000;
+    int a3 = ((a1%100)/1)%10;
+    Console.WriteLine($"{number} -> {a3}");
+}
+else if (a1 > 100000)
+{
+    a1 = number/1000;
+    int a3 = ((a1%100)/1)%10;
+    Console.WriteLine($"{number} -> {a3}");
+}
+else if (a1 > 10000)
+{
+    a1 = number/100;
+    int a3 = ((a1%100)/1)%10;
+    Console.WriteLine($"{number} -> {a3}");
+}
+else if (a1 > 1000)
+{
+    a1 = number/10;
+    int a3 = ((a1%100)/1)%10;
+    Console.WriteLine($"{number} -> {a3}");
+}
+else if (a1 > 100)
+{
+    a1 = number/1;
+    int a3 = ((a1%100)/1)%10;
+    Console.WriteLine($"{number} -> {a3}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
