a27e7ccbb0e6628091d7aac7b8f8a04cf74497ae
AlexSyrkin <hellius2031@mail.ru>
2022-10-15 14:12:54 +0200

    add Homework Seminar 2 Task 13

--- Homework_Seminar2_Task_13/Program.cs
+++ Homework_Seminar2_Task_13/Program.cs
@@ -21,37 +21,37 @@ else if (a1 > 100000000)
 else if (a1 > 10000000)
 {
     a1 = number/100000;
-    int a3 = ((a1%100)/1)%10;
+    int a3 = (a1%100)%10;
     Console.WriteLine($"{number} -> {a3}");
 }
 else if (a1 > 1000000)
 {
     a1 = number/10000;
-    int a3 = ((a1%100)/1)%10;
+    int a3 = (a1%100)%10;
     Console.WriteLine($"{number} -> {a3}");
 }
 else if (a1 > 100000)
 {
     a1 = number/1000;
-    int a3 = ((a1%100)/1)%10;
+    int a3 = (a1%100)%10;
     Console.WriteLine($"{number} -> {a3}");
 }
 else if (a1 > 10000)
 {
     a1 = number/100;
-    int a3 = ((a1%100)/1)%10;
+    int a3 = (a1%100)%10;
     Console.WriteLine($"{number} -> {a3}");
 }
 else if (a1 > 1000)
 {
     a1 = number/10;
-    int a3 = ((a1%100)/1)%10;
+    int a3 = (a1%100)%10;
     Console.WriteLine($"{number} -> {a3}");
 }
 else if (a1 > 100)
 {
     a1 = number/1;
-    int a3 = ((a1%100)/1)%10;
+    int a3 = (a1%100)%10;
     Console.WriteLine($"{number} -> {a3}");
 }
 
