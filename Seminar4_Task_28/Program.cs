d53b106fbae6cc0abee24e8a250d8a7053a30cc2
AlexSyrkin <hellius2031@mail.ru>
2022-10-26 20:12:19 +0200

    add Seminar 4 Task 28

---
+++ Seminar4_Task_28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(NumberProd(num));
+
+int NumberProd(int number)
+{
+    int prod = 1;
+    while(number > 1)
+    {
+        prod = prod * number;
+        Console.WriteLine($"число {number} = {prod} * {number} = {prod}");
+        number--;
+        Console.WriteLine(number);
+    }
+    return prod;
+}
