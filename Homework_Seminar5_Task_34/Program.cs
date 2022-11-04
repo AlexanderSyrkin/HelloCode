bd8b2feef05a8a08109b55bd3642b9fb299f9d6c
AlexSyrkin <hellius2031@mail.ru>
2022-11-04 20:35:45 +0100

    add Homework Seminar Task 34

---
+++ Homework_Seminar5_Task_34/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+int[] array = new int[4];
+GetArray(array);
+Chetnoe(array);
+
+
+void GetArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++) 
+    {
+        array[i] = new Random().Next(99, 999); 
+    }
+Console.Write("[{0}] ", string.Join(", ", array));
+}
+
+void Chetnoe(int[] array)
+{
+    int chetnoe = 0;
+    foreach (int el in array)
+    {
+        if (el % 2 == 0)
+        chetnoe++; 
+    }
+Console.WriteLine($" -> {chetnoe}.");
+}
+
+
+
+
+
+
+
+
+
+/* int chetnoe = 0;
+    
+foreach (int el in array)
+{
+    if (el % 2 == 0)
+        chetnoe++; 
+}
+
+Console.WriteLine($" -> {chetnoe}.");
+ */
+
+
