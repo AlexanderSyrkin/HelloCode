16719cc984a02fb836cb5dc762859aa8c9ba0724
AlexSyrkin <hellius2031@mail.ru>
2022-11-05 01:42:47 +0100

    add Homework Seminar 5 Task 38

---
+++ Homework_Seminar5_Task_38/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+double[] array = new double[7];
+GetArray(array);
+MinMaxValue(array);
+
+void GetArray(double[] array)
+{
+    Random rand = new Random();
+    for(int i = 0; i < array.Length; i++) 
+    {
+        array[i] = 100- rand.Next(100) + rand.NextDouble();
+    }
+Console.Write("[{0}] ", string.Join(", ", array));
+}
+
+   
+void MinMaxValue(double[] array)
+{  
+    double dif = 0;
+    int imin = 0, imax = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > array[imax])
+        {
+            imax = i;
+        }
+        if (array[i] < array[imin])
+        {
+            imin = i;
+        }
+    }
+    double diff = array[imax] - array [imin];
+Console.WriteLine($"-> {Math.Round(diff,2)}");
+Console.WriteLine();
+Console.WriteLine("min value: array[" + imin + "] = " + Math.Round(array[imin],2));
+Console.WriteLine("max value: array[" + imax + "] = " + Math.Round(array[imax],2));
+Console.ReadLine();
+}
+
+
