331961a8aa9748faf7e69145d0de709733432300
AlexSyrkin <hellius2031@mail.ru>
2022-11-05 13:53:46 +0100

    add Homework Seminar 5 Task 41

---
+++ Homework_Seminar5_Task_41/Program.cs
@@ -0,0 +1,34 @@
+﻿// // Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+
+void Main()
+{
+    Console.Clear();
+    int[] array = NewArray();
+    int count = Count(array);
+    Console.WriteLine($"Кол-во чисел больше нуля: {count}.");
+}
+
+int[] NewArray()
+{
+    Console.Write("Введите любые числа через пробел: ");
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    return arr;
+}
+
+int Count(int[] arr)
+{
+    int count = 0;
+ 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Main();
