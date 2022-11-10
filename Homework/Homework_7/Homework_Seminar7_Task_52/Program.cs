2bbc442fb95d51109a8b58459e56e3660423c707
AlexSyrkin <hellius2031@mail.ru>
2022-11-10 23:28:09 +0200

    add Homework Seminar 7 Task 52

---
+++ Homework/Homework_7/Homework_Seminar7_Task_52/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+//  Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void Main()
+{
+    Console.Clear();
+    Console.Write("Введите количество строк: ");
+    int rows = int.Parse(Console.ReadLine());
+    
+    Console.Write("Введите количество столбцов: ");
+    int columns = int.Parse(Console.ReadLine());
+    int[,] array = new int[rows, columns];
+    
+    GetArray(array);
+    PrintArray(array);
+    Console.WriteLine();
+    AvergeInColumns(rows, array);
+}
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AvergeInColumns(int n, int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double avarage = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            avarage = (avarage + array[i, j]);
+        }
+        avarage = avarage / n;
+        Console.Write(Math.Round(avarage, 2) + "; ");
+    }
+    Console.WriteLine();
+}
+
+Main();
