45c9b22641ffe8085c7ff468df98325edfd73f1b
AlexSyrkin <hellius2031@mail.ru>
2022-11-13 19:09:19 +0200

    Seminar 8 Task 57

---
+++ Seminars/Seminar_8/Seminar_8_Task57/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 57: Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент входных данных.
+
+
+void Main()
+{
+    Console.Clear();
+    Console.Write("Введите количество строк: ");
+    int rows = int.Parse(Console.ReadLine());
+
+    Console.Write("Введите количество столбцов: ");
+    int columns = int.Parse(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    GetArray(array);
+    PrintArray(array);
+    for (int z = 1; z < 10; z++)
+    {
+        int countNumber = CountNum(array, z);
+        if (countNumber > 0)
+        {
+            Console.WriteLine($"Число {z} всречается: {countNumber} раз.");
+        }        
+    }
+}
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j] + " "}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int CountNum(int[,] array, int number)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == number) count++;
+        }
+    }
+    return count;
+}
+
+Main();
